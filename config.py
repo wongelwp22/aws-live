e2f530ae46d9886b84cc39a2e3cc8fb86614056b
ling <121840973+wongelwp22@users.noreply.github.com>
2025-08-11 08:43:37 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-customhost = "database-1.chauwtk5goof.us-east-1.rds.amazonaws.com"
+customhost = "employee.cpd1zussdoq6.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
 customdb = "employee"
-custombucket = "lowchoonkeat-employee1"
+custombucket = "wongeuling-employee"
 customregion = "us-east-1"
 
